50ae78fdcbd2d0a2e1b326be4c0bbacd09742298
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2023-01-21 01:26:31 +0800

    Designing initial phase of UI.

--- app/src/main/java/com/araa/dravio/ui/view/MainActivity.kt
+++ app/src/main/java/com/araa/dravio/ui/view/MainActivity.kt
@@ -3,10 +3,15 @@ package com.araa.dravio.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.araa.dravio.R
+import com.araa.dravio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
